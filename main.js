925ef70f757b72521ccafe817e2b6f22df8fee57
RS6 <88463690+Syndrosia@users.noreply.github.com>
2023-09-12 00:41:06 +0100

    Update main.js

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ let Mainlb = 0;
 let Mainoz = 0;
 let Maindra = 0;
 
+function print(a) {
+   console.log(a);
+}
+
 function calc() {
    Maxlb = document.getElementById("Maxlb").value;
    Maxoz = document.getElementById("Maxoz").value;
@@ -15,19 +19,29 @@ function calc() {
 
    let x = (Maxlb * 25600) + (Maxoz * 1600) + (Maxdra * 100);
    let y = (Mainlb * 25600) + (Mainoz * 1600) + (Maindra * 100);
-   let z = ((y/x) * 100).toFixed(1)
+   let z = ((y/x) * 100).toFixed(1);
 
-   document.getElementById("perc").innerHTML = z + "%"
+   document.getElementById("perc").innerHTML = z + "%";
+   
+   let row = document.getElementById("tal1").insertRow(0);
+   let Amax = row.insertCell(0);
+   let row2 = document.getElementById("tal2").insertRow(0);
+   let Amain = row2.insertCell(0);
+   let row3 = document.getElementById("tal3").insertRow(0);
+   let Aperc = row3.insertCell(0);
 
-   x = 0
-   y = 0
-   z = 0
+   Amax.innerHTML = String(Maxlb) + ":" + String(Maxoz) + ":" + String(Maxdra);
+   Amain.innerHTML = String(Mainlb) + ":" + String(Mainoz) + ":" + String(Maindra);
+   Aperc.innerHTML = String(z) + "%";
 
-   document.getElementById("Maxlb").value = ""
-   document.getElementById("Maxoz").value = ""
-   document.getElementById("Maxdra").value = ""
-   document.getElementById("Mainlb").value = ""
-   document.getElementById("Mainoz").value = ""
-   document.getElementById("Maindra").value = ""
+   x = 0;
+   y = 0;
+   z = 0;
 
+   document.getElementById("Maxlb").value = "";
+   document.getElementById("Maxoz").value = "";
+   document.getElementById("Maxdra").value = "";
+   document.getElementById("Mainlb").value = "";
+   document.getElementById("Mainoz").value = "";
+   document.getElementById("Maindra").value = "";
 }
